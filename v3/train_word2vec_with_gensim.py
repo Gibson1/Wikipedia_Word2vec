4bde82b4c9d9a31cb71373a63003bb2f715b06a6
Gibson Chen <gibson.chen@acxiom.com>
2018-07-04 14:30:36 +0800

    add python 3 version

---
+++ v3/train_word2vec_with_gensim.py
@@ -0,0 +1,99 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Author: Pan Yang (panyangnlp@gmail.com)
+# Copyright 2017 @ Yu Zhen
+
+import gensim
+import logging
+import multiprocessing
+import os
+import re
+import sys
+
+from time import time
+import imp
+
+logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s',
+                    level=logging.INFO)
+
+
+def cleanhtml(raw_html):
+    cleanr = re.compile('<.*?>')
+    cleantext = re.sub(cleanr, ' ', raw_html)
+    return cleantext
+
+
+class MySentences(object):
+    def __init__(self, dirname):
+        self.dirname = dirname
+
+    def __iter__(self):
+        for root, dirs, files in os.walk(self.dirname):
+            for filename in files:
+                file_path = root + '/' + filename
+                for line in open(file_path):
+                    sline = line.strip()
+                    if sline == "":
+                        continue
+                    rline = cleanhtml(sline)
+                    # tokenized_line = ' '.join(tokenize(rline))
+                    tokenized_line = rline
+                    if lang == 'en':
+                        is_valid_word_line = [word for word in
+                                              tokenized_line.lower().split()
+                                              if word.isalpha()]
+                    else:
+                        is_valid_word_line = [word for word in
+                                              tokenized_line.lower().split()]
+                    yield is_valid_word_line
+
+
+if __name__ == '__main__':
+    if len(sys.argv) < 3:
+        print("Please use python train_with_gensim.py data_path out_path")
+        exit(255)
+    data_path = sys.argv[1]
+    out_path = sys.argv[2]
+
+    # check parameters
+    if not os.path.exists(data_path):
+        print("[data_path] does not exist. Please try again with valid data_path")
+        exit(254)
+
+    # prepare env
+    imp.reload(sys)
+    # sys.setdefaultencoding('utf-8')
+    sys.getfilesystemencoding = lambda: 'UTF-8'
+    imp.reload(sys)
+
+    # define constants
+    rootdir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+    jobname = os.path.splitext(os.path.basename(__file__))[0]
+    out_file = os.path.abspath(out_path) + os.path.sep + os.path.basename(data_path) + '.model.word2vec'
+    lang = 'en'
+    if len(sys.argv) > 3:
+        lang = sys.argv[3]
+
+    begin = time()
+
+    sentences = MySentences(data_path)
+    model = gensim.models.Word2Vec(sentences,
+                                   size=200,
+                                   window=10,
+                                   min_count=10,
+                                   workers=multiprocessing.cpu_count())
+
+    if not os.path.exists(out_path):
+        try:
+            oldumask = os.umask(0)
+            os.makedirs(out_path, 0o775)
+        finally:
+            os.umask(oldumask)
+
+    model.save(out_file + "_gensim")
+    model.wv.save_word2vec_format(out_file + "_org",
+                                  out_file + "_vocabulary",
+                                  binary=False)
+
+    end = time()
+    print("Total procesing time: %d seconds" % (end - begin))
